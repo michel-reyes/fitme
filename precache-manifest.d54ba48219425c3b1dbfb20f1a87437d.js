afeb6ca2cfffc5e563f257616c96a0490b0f3318
Michel Reyes <mreyesh85@gmail.com>
2019-06-05 00:05:14 -0400

    Updates

---
+++ precache-manifest.d54ba48219425c3b1dbfb20f1a87437d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af4fcc65fc6e0f5be5acd9f348fc6efe",
+    "url": "/fitme/index.html"
+  },
+  {
+    "revision": "5f5f28c6dd5dd801eb12",
+    "url": "/fitme/static/css/main.4bd6fc45.chunk.css"
+  },
+  {
+    "revision": "b1b266b4b9628df88afa",
+    "url": "/fitme/static/js/2.50d2f1a1.chunk.js"
+  },
+  {
+    "revision": "5f5f28c6dd5dd801eb12",
+    "url": "/fitme/static/js/main.86a5010c.chunk.js"
+  },
+  {
+    "revision": "69a926689573685909f1",
+    "url": "/fitme/static/js/runtime~main.8fda4134.js"
+  }
+]);
